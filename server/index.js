052713b4e99bdbac7915f6164265f83ea23e8fcc
Moses Kim <mosesrandykim@gmail.com>
2021-07-23 18:08:18 +0000

    updated server/index.js to handle GET requests for the /api/location route. In the route, I was able to send a GET request with the lat and lng provides in the query string and tested the server using httpie.

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const yelp = require('yelp-fusion');
 const client = yelp.client(process.env.YELP_API_KEY);
+const fetch = require('node-fetch');
 const app = express();
 
 app.use(staticMiddleware);
@@ -34,4 +35,17 @@ app.get('/api/businesses', (req, res, next) => {
     }
     next(err);
   });
+});
+
+app.get(('/api/location'), (req, res, next) => {
+  const { lat, lng } = req.query;
+  if (!lat || !lng) {
+    throw new ClientError(400, 'latitude and longitude are required');
+  }
+  fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      res.status(200).send(data);
+    })
+    .catch(err => next(err));
 });
