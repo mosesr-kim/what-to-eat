603409cfbccd636895dcab383735decb60da98ac
Moses Kim <mosesrandykim@gmail.com>
2021-07-23 23:40:04 +0000

    made sure that when the data from the server was getting stored in the state of the details page

--- client/pages/details.jsx
+++ client/pages/details.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 export default class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businessDetails: null,
+      businessReviews: null
+    };
+  }
+
   componentDidMount() {
     fetch(`/api/business?businessId=${this.props.businessId}`)
       .then(response => response.json())
       .then(data => {
-        // console.log(data);
+        this.setState({
+          businessDetails: data.businessDetails,
+          businessReviews: data.businessReviews
+        });
       });
   }
 
