f7f9ec21016bac70fa47d5954d1a2f61c2802902
Moses Kim <mosesrandykim@gmail.com>
2021-07-23 18:40:54 +0000

    created a plat and lng property in the state of the app.jsx component

--- client/app.jsx
+++ client/app.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Home from './pages/home';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      lng: null
+    };
+  }
+
   render() {
     return <Home />;
   }
