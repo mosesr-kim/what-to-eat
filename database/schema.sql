f3bf1d0b9fd9ac4c2f6c9d3b86b06fd638ed7f7b
Moses Kim <mosesrandykim@gmail.com>
2021-07-26 17:42:02 +0000

    added a schema for users and collections

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,28 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"username" TEXT NOT NULL,
+	"password" TEXT NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "public.Collections" (
+	"collectionId" serial NOT NULL,
+	"userId" integer NOT NULL,
+	"createdAt" timestamp with time zone NOT NULL,
+	CONSTRAINT "Collections_pk" PRIMARY KEY ("collectionId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+ALTER TABLE "Collections" ADD CONSTRAINT "Collections_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
