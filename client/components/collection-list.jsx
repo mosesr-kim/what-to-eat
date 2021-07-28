cf274b5a8b11f6dcb3a563612c6facf5fa043cea
Moses Kim <mosesrandykim@gmail.com>
2021-07-28 19:53:22 +0000

    styled the collection list. still need to add the save button component

--- client/components/collection-list.jsx
+++ client/components/collection-list.jsx
@@ -9,22 +9,21 @@ export default class CollectionList extends React.Component {
   render() {
     if (!this.props.collections) return null;
     const collectionList = this.props.collections.map((collection, index) => {
+      const image = collection.image ? '' : 'hidden';
       return (
-        <>
-          <li key={collection.collectionId} className="collectionLi">
-            <div className="collectionImageColumn">
-              <img src={collection.image} alt={`${collection.name} image`} className="collectionImage" />
-            </div>
-            <div className="collectionTextColumn">
-              <h3 className="collectionNameText">
-                {collection.name}
-              </h3>
-              <h3 className="collectionCountText">
-                {collection.count} places saved
-              </h3>
-            </div>
-          </li>
-        </>
+        <li key={collection.collectionId} className="collectionLi">
+          <div className="collectionImageColumn">
+            <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
+          </div>
+          <div className="collectionTextColumn">
+            <h3 className="collectionNameText">
+              {collection.name}
+            </h3>
+            <h3 className="collectionCountText">
+              {collection.count} places saved
+            </h3>
+          </div>
+        </li>
       );
     });
     return (
