620c90dc8d7761ca20ff4463710977c901d0f64f
Moses Kim <mosesrandykim@gmail.com>
2021-07-29 21:42:32 -0700

    added a conditional in my map method that checks if the user is saving, if the user is saving, the anchor tag to redirect the user to the collection page is not rendered, but if they are not, the anchor tag is rendered

--- client/components/collection-list.jsx
+++ client/components/collection-list.jsx
@@ -6,9 +6,9 @@ export default class CollectionList extends React.Component {
     if (!this.props.collections) return null;
     const collectionList = this.props.collections.map((collection, index) => {
       const image = collection.image ? '' : 'hidden';
-      return (
-        <a href={`#collection?collectionId=${collection.collectionId}`} key={collection.collectionId} onClick={this.props.handleClick} >
-          <li className="collectionLi">
+      if (this.props.saving) {
+        return (
+          <li className="collectionLi" key={collection.collectionId}>
             <div className="collectionImageColumn">
               <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
             </div>
@@ -22,8 +22,27 @@ export default class CollectionList extends React.Component {
             </div>
             <SaveButton saving={this.props.saving} collectionId={collection.collectionId} handleClick={this.props.handleClick} handleSave={this.props.handleSave} />
           </li>
-        </a>
-      );
+        );
+      } else {
+        return (
+          <a href={`#collection?collectionId=${collection.collectionId}`} key={collection.collectionId} >
+            <li className="collectionLi">
+              <div className="collectionImageColumn">
+                <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
+              </div>
+              <div className="collectionTextColumn">
+                <h3 className="collectionNameText">
+                  {collection.name}
+                </h3>
+                <h3 className="collectionCountText">
+                  {collection.count} places saved
+                </h3>
+              </div>
+              <SaveButton saving={this.props.saving} collectionId={collection.collectionId} handleClick={this.props.handleClick} handleSave={this.props.handleSave} />
+            </li>
+          </a>
+        );
+      }
     });
     return (
       <>
