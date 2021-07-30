950861389c243dee8551a054587a1c04c46b1bb2
Moses Kim <mosesrandykim@gmail.com>
2021-07-30 18:11:47 +0000

    added a ternary exxpression to render the save button or an anchor tag that says view if the user is saving or just viewing.

--- client/components/collection-list.jsx
+++ client/components/collection-list.jsx
@@ -6,43 +6,25 @@ export default class CollectionList extends React.Component {
     if (!this.props.collections) return null;
     const collectionList = this.props.collections.map((collection, index) => {
       const image = collection.image ? '' : 'hidden';
-      if (this.props.saving) {
-        return (
-          <li className="collectionLi" key={collection.collectionId}>
-            <div className="collectionImageColumn">
-              <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
-            </div>
-            <div className="collectionTextColumn">
-              <h3 className="collectionNameText">
-                {collection.name}
-              </h3>
-              <h3 className="collectionCountText">
-                {collection.count} places saved
-              </h3>
-            </div>
-            <SaveButton saving={this.props.saving} collectionId={collection.collectionId} handleClick={this.props.handleClick} handleSave={this.props.handleSave} />
-          </li>
-        );
-      } else {
-        return (
-          <a href={`#collection?collectionId=${collection.collectionId}`} key={collection.collectionId} >
-            <li className="collectionLi">
-              <div className="collectionImageColumn">
-                <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
-              </div>
-              <div className="collectionTextColumn">
-                <h3 className="collectionNameText">
-                  {collection.name}
-                </h3>
-                <h3 className="collectionCountText">
-                  {collection.count} places saved
-                </h3>
-              </div>
-              <SaveButton saving={this.props.saving} collectionId={collection.collectionId} handleClick={this.props.handleClick} handleSave={this.props.handleSave} />
-            </li>
-          </a>
-        );
-      }
+      return (
+        <li className="collectionLi" key={collection.collectionId}>
+          <div className="collectionImageColumn">
+            <img src={collection.image} alt={`${collection.name} image`} className={`collectionImage ${image}`} />
+          </div>
+          <div className="collectionTextColumn">
+            <h3 className="collectionNameText">
+              {collection.name}
+            </h3>
+            <h3 className="collectionCountText">
+              {collection.count} places saved
+            </h3>
+          </div>
+          { this.props.saving
+            ? <SaveButton saving={this.props.saving} collectionId={collection.collectionId} handleClick={this.props.handleClick} handleSave={this.props.handleSave} />
+            : <a href={`#collection? collectionId = ${collection.collectionId}`} className="viewAnchor">View</a>
+          }
+        </li>
+      );
     });
     return (
       <>
